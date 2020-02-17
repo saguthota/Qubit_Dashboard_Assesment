23eb4aa2d49373c1b4bb4621e126d81851d02152
saguthota <saguthota@gmail.com>
2020-02-17 18:58:28 +0530

    InitialCommit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: '/user-profile', title: 'User Details',  icon:'', class: '' },
+    { path: '/dashboard', title: 'Dashboard',  icon: '', class: '' },    
+    { path: '/table-list', title: 'Rainfall_Table',  icon:'', class: '' },
+    { path: '/notifications', title: 'Whether_Notifications',  icon:'', class: '' },
+    ];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../dashboard/dashboard.component';
+import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { TableListComponent } from '../../table-list/table-list.component';
+import { NotificationsComponent } from '../../notifications/notifications.component';
+
+export const AdminLayoutRoutes: Routes = [
+    
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'table-list',     component: TableListComponent },
+    { path: 'notifications',  component: NotificationsComponent },
+];
